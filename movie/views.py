023d035b66e86bbb55c9f88d9be2c42d786c2b26
junio14245252626236 <acevedomagally1@gmail.com>
2025-07-31 16:15:29 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Movie
 
 # Create your views here.
 
@@ -8,7 +8,13 @@ from django.http import HttpResponse
 def home(request):
  #return HttpResponse('<h1> Welcome to HOME PAGE</h1>')
  #return render(request, 'home.html')
- return render(request, 'home.html', {'name': 'Emilton Mena'})
+ #return render(request, 'home.html', {'name': 'Emilton Mena'})
+ searchTerm = request.GET.get('searchMovie')
+ if searchTerm:
+     movies = Movie.objects.filter(title__icontains=searchTerm)
+ else:
+    movies = Movie.objects.all() 
+ return render(request, 'home.html', {'movies': movies, 'searchTerm': searchTerm})
 
 
 # Página About
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -126,6 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
